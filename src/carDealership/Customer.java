8e82bdc7b7177dcaf98e3533058f3c84dece4d2f
myztajay <myztajay@hotmail.com>
2017-11-01 19:07:26 -0400

    Added needed classes the coded properties for customers

---
+++ src/carDealership/Customer.java
@@ -0,0 +1,37 @@
+package carDealership;
+
+public class Customer {
+
+	private String name;
+	private String address;
+	private int cash;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getCash() {
+		return cash;
+	}
+
+	public void setCash(int cash) {
+		this.cash = cash;
+	}
+
+	public void purchaseCar(Vehicle vehicle, Employee emp, boolean finance) {
+		
+	}
+
+}
